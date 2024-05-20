6fd331d38b65fe796060d6f28a38ce5292e95410
migue02ax <axel_fuster_barba@hotmail.com>
2024-05-20 13:41:17 -0600

    conexion_infinity

--- conexion.php
+++ conexion.php
@@ -5,10 +5,10 @@ $dbuser= 'root';
 $dbhost='127.0.0.1:3308';
 $dbpass= ''; */
 
-/* $dbname='if0_36150765_pruebitas';
+$dbname='if0_36150765_proyecto	';
 $dbuser= 'if0_36150765';
 $dbhost='sql101.infinityfree.com';
-$dbpass= 'sgk6ggKWx0';  */
+$dbpass= 'sgk6ggKWx0'; 
 
 
 
