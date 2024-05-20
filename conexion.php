e323b93fa43bb1c2194d02dd678b7cdfc3cf4634
migue02ax <axel_fuster_barba@hotmail.com>
2024-05-20 12:37:11 -0600

    creacion_conexion

---
+++ conexion.php
@@ -0,0 +1,18 @@
+<?php
+
+/* $dbname='spiderman';
+$dbuser= 'root';
+$dbhost='127.0.0.1:3308';
+$dbpass= ''; */
+
+/* $dbname='if0_36150765_pruebitas';
+$dbuser= 'if0_36150765';
+$dbhost='sql101.infinityfree.com';
+$dbpass= 'sgk6ggKWx0';  */
+
+
+
+
+$conexion= mysqli_connect($dbhost,$dbuser,$dbpass, $dbname);
+
+?>
